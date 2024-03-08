eb1021fb6c7f0955f9a18016a0f1f21faa3acb61
marcos bonet <marcos.bonet@es.abb.com>
2024-03-08 12:24:30 +0100

    Requirements Analysis and Domain model

---
+++ src/models/classes/classCompany.ts
@@ -0,0 +1,20 @@
+import { CompanyStorage } from "../class";
+
+export class Company {
+  _symbol: string;
+  _name: string;
+
+  constructor(symbol: string, name: string, companyStorage: CompanyStorage) {
+    if (!/^[a-zA-Z0-9]{2,5}$/.test(symbol)) {
+      throw new Error(
+        "The company symbol must be between 2 and 5 alphanumeric characters."
+      );
+    }
+    if (companyStorage.doesCompanyExistWithSymbol(symbol)) {
+      throw new Error("A company with this symbol already exists.");
+    }
+    this._symbol = symbol;
+    this._name = name;
+    companyStorage.addCompany(this);
+  }
+}
